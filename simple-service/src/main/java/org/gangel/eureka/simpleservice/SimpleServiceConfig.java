217d82bcf77478adedc723d3573b7df02186ebea
Grzegorz Aniol <Grzegorz_Aniol@epam.com>
2017-09-13 14:08:56 +0200

    Add @RefreshScope for testing properties change

--- simple-service/src/main/java/org/gangel/eureka/simpleservice/SimpleServiceConfig.java
+++ simple-service/src/main/java/org/gangel/eureka/simpleservice/SimpleServiceConfig.java
@@ -1,10 +1,12 @@
 package org.gangel.eureka.simpleservice;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix="some")
+@RefreshScope
 public class SimpleServiceConfig {
     
     private String property;
