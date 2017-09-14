47c93fc2703f34ea008850fc0ca7b850cf45c516
Grzegorz Aniol <Grzegorz_Aniol@epam.com>
2017-09-14 14:59:06 +0200

    Enable actuators

--- simple-service/src/main/java/org/gangel/eureka/simpleservice/SimpleServiceApplication.java
+++ simple-service/src/main/java/org/gangel/eureka/simpleservice/SimpleServiceApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableWebSecurity
 @RestController
 @RefreshScope
 public class SimpleServiceApplication {
