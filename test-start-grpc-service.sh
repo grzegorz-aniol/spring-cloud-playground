292c2d34db8d60602196f3083403f60b40dcc372
Grzegorz Aniol <Grzegorz_Aniol@epam.com>
2017-12-05 18:04:20 +0100

    Use default H2 database. Add new profile for H2 database

--- test-start-grpc-service.sh
+++ test-start-grpc-service.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo host=$TEST_SERVER_HOST
+set SPRING_DATASOURCE_URL=jdbc:mysql://EPPLKATW0022.budapest.epam.com:3306/orders?serverTimezone=UTC
 
 #nohup 
 java -Xmx500m -Xbootclasspath/p:alpn-boot.jar -jar ./bin/orders-grpc-service-1.0-SNAPSHOT.jar --ssl.certFileName=${TEST_CERT_FILE} --ssl.privKeyFileName=${TEST_KEY_FILE} --grpc.host=$TEST_SERVER_HOST
--- test-start-http-service.sh
+++ test-start-http-service.sh
@@ -2,7 +2,7 @@
 echo host=$TEST_SERVER_HOST
 
 #nohup 
-java -Xmx500m -Xbootclasspath/p:alpn-boot.jar -jar ./bin/orders-http-service-0.0.1-SNAPSHOT.jar --spring.profiles.active=default,mysql,ssl --server.ssl.key-store=${TEST_KEYSTORE_NAME}.jks --server.ssl.key-store-password=${TEST_JKS_PASS} --server.ssl.key-password=${TEST_JKS_PASS} --server.host=$TEST_SERVER_HOST
+java -Xmx500m -Xbootclasspath/p:alpn-boot.jar -jar ./bin/orders-http-service-0.0.1-SNAPSHOT.jar --spring.profiles.active=default,ssl --server.ssl.key-store=${TEST_KEYSTORE_NAME}.jks --server.ssl.key-store-password=${TEST_JKS_PASS} --server.ssl.key-password=${TEST_JKS_PASS} --server.host=$TEST_SERVER_HOST
 
 
 ## java -jar orders-http-service-0.0.1-SNAPSHOT.jar --spring.profiles.active=default,mysql,ssl --server.ssl.key-store="c:/Users/Grzegorz_Aniol/OneDrive - EPAM/Secured/spring-cloud-playground/orders-https-service/keystore.jks" --server.ssl.key-store-password=java --server.ssl.key-password=java
