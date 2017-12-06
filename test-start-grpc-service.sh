a888c18fcd6ffa6af250d7c70e5ee1b55b28afaf
Grzegorz Aniol <Grzegorz_Aniol@epam.com>
2017-12-06 15:05:40 +0100

    Clean scripts for tests. Add performance results from corporate cloud and AWS

--- test-start-grpc-service.sh
+++ test-start-grpc-service.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-echo Host: $TEST_SERVER_HOST
-set SPRING_DATASOURCE_URL=jdbc:mysql://EPPLKATW0022.budapest.epam.com:3306/orders?serverTimezone=UTC
 
-#nohup 
-java -Xmx500m -Xbootclasspath/p:alpn-boot.jar -jar ./bin/orders-grpc-service-1.0-SNAPSHOT.jar --ssl.certFileName=${TEST_CERT_FILE} --ssl.privKeyFileName=${TEST_KEY_FILE} --grpc.host=$TEST_SERVER_HOST
+java -Xmx1g -Xbootclasspath/p:alpn-boot.jar -jar ./bin/orders-grpc-service-1.0-SNAPSHOT.jar --ssl.certFileName=${TEST_CERT_FILE} --ssl.privKeyFileName=${TEST_KEY_FILE} --grpc.host=$TEST_SERVER_HOST
 
